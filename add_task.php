e781158acf3db3567afebf5e85c1e3ef1ba85645
Sonal Hans <sonalhans05@gmail.com>
2024-11-01 18:27:36 +0530

    styles added

--- add_task.php
+++ add_task.php
@@ -21,11 +21,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Add Task</title>
 </head>
 
-<body class="bg-gray-50">
-    <h1 class="text-center text-4xl font-serif mt-5">Add New Task </h1>
+<body class="bg-violet-50">
+
+    <h1 class="text-center text-4xl font-serif my-5">Add New Task </h1>
     <form method="post" action="">
 
-        <div class="mx-60">
+        <div class="mx-60 block shadow-lg p-6 rounded-xl bg-violet-100">
             <label for="Title" class="block text-sm/6 font-medium text-gray-900">Title</label>
             <div class="mt-2">
                 <input name="title" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300  focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" required>
@@ -45,7 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <div class="mt-5">
 
-                <button type="submit" class="rounded-full bg-red-400 px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Add Task</button>
+                <button type="submit" class="rounded-full bg-violet-300 px-4 py-2.5 text-sm font-semibold text-violet-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Add Task</button>
             </div>
 
         </div>
@@ -54,6 +55,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
     </form>
+    </div>
+
     <script src="https://cdn.tailwindcss.com"></script>
 </body>
 
